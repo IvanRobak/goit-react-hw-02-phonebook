3e619db0d556a97916d0be08bd94f52abdf254c1
ivan1810 <78267912+ivan1810@users.noreply.github.com>
2023-04-30 11:24:59 +0300

    add ContactList

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,23 +1,55 @@
 import { Component } from 'react';
 import ContactForm from './Form';
+// import { nanoid } from 'nanoid';
+import ContactList from './ContactList/ContactList';
 export class App extends Component {
-  state = {};
-  submitForm = data => {
-    console.log(data);
+  state = {
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
+    filter: '',
   };
+
+  submitForm = ({ id, name, number }) => {
+    const contact = {
+      id,
+      name,
+      number,
+    };
+    this.setState(({ contacts }) => ({
+      contacts: [contact, ...contacts],
+    }));
+  };
+  handeDeleteContact = deleteId => {
+    this.setState(prevState => ({
+      ...prevState,
+      contacts: this.state.contacts.filter(({ id }) => id !== deleteId),
+    }));
+  };
+
   render() {
+    const { contacts } = this.state;
     return (
       <div
         style={{
           height: '100vh',
           display: 'flex',
+          flexDirection: 'column',
           justifyContent: 'center',
           alignItems: 'center',
           fontSize: 40,
           color: '#010101',
         }}
       >
-        <ContactForm onSubmit={this.submitForm} />
+        <ContactForm onSubmit={this.submitForm} contactList={contacts} />
+        <h2 title="Filter">Filter</h2>
+        <ContactList
+          contactList={contacts}
+          onDelete={this.handeDeleteContact}
+        />
       </div>
     );
   }
